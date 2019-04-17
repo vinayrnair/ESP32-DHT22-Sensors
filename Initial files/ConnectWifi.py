77e6cf489187f84681e80ab5a304b966d98640f9
vinayrnair <33867963+vinayrnair@users.noreply.github.com>
2019-04-17 14:40:52 +0200

    Create ConnectWifi.py

---
+++ Initial files/ConnectWifi.py
@@ -0,0 +1,20 @@
+def connect():
+    import network
+ 
+    ssid = "username"
+    password = "password"
+ 
+    station = network.WLAN(network.STA_IF)
+ 
+    if station.isconnected() == True:
+        print("Already connected")
+        return
+ 
+    station.active(True)
+    station.connect(ssid, password)
+ 
+    while station.isconnected() == False:
+        pass
+ 
+    print("Connection successfull")
+    print(station.ifconfig())
